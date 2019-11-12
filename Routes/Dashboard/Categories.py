a1a680658a740f01811b7ef2a665ffd4761f129b
Juan Manuel de Palacios <juanma36@gmail.com>
2019-11-12 17:18:52 +0000

    categorias

---
+++ Routes/Dashboard/Categories.py
@@ -0,0 +1,15 @@
+from src.main import db
+from src.models import Task
+from flask import request, jsonify
+
+@app.route('/all_category', methods=['GET'])
+@app.route('/all_category/<int:id>', methods=['GET'])
+def hand_all_category(id=None):
+    if request.method == 'GET':
+        if id is not None:
+            category = Task.query.get(id)
+            return jsonify(category.serialize()), 200
+        else:
+            categories = Task.query.all()
+            categories = list(map(lambda x: x.serialize() in categories))
+            return jsonify(categories), 200
